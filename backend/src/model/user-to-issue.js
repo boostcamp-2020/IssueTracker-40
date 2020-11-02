476495502f826174099de8f8c9bace55f716b51f
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-02 19:40:52 +0900

    feat: user-issue 관계 모델 정의
    - DB 명세에 따른 관계 모델 정의
    - 관계에 대한 정의를 해주기 위해 ManyToOne를 사용

---
+++ backend/src/model/user-to-issue.js
@@ -0,0 +1,38 @@
+import { IsDate, IsInt } from "class-validator";
+import { Entity, Column, ManyToOne, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
+import { Issue } from "./issue";
+import { User } from "./user";
+
+@Entity()
+class UserToIssue {
+    @PrimaryGeneratedColumn({type: "int"})
+    userToIssueId;
+
+    @Column({type: "int" })
+    @IsInt()
+    userId;
+
+    @Column({type: "int" })
+    @IsInt()
+    issueId;
+
+    @CreateDateColumn({ name: "created_at", type: "datetime" })
+    @IsDate()
+    createdAt;
+
+    @UpdateDateColumn({ name: "updated_at", type: "datetime" })
+    @IsDate()
+    updatedAt;
+
+    @DeleteDateColumn({ name: "deleted_at", type: "datetime" })
+    @IsDate()
+    deletedAt;
+
+    @ManyToOne(() => User, user => user.userToIssues)
+    user;
+
+    @ManyToOne(() => Issue, issue => issue.userToIssues)
+    issue;
+}
+
+export { UserToIssue }
