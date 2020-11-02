f6d4ad3cea2edacc3a6e0c887d04fcf1f407c817
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-02 19:39:37 +0900

    feat: comment 모델 정의
    - DB 명세에 따른 모델 정의
    - 관계에 대한 정의를 해주기 위해 ManyToOne를 사용

---
+++ backend/src/model/comment.js
@@ -0,0 +1,35 @@
+import { IsDate, IsString, IsUrl } from "class-validator";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, ManyToOne, DeleteDateColumn } from "typeorm";
+import { Issue } from "./issue";
+import { User } from "./user";
+
+@Entity({ name: "comment" })
+class Comment {
+    @PrimaryGeneratedColumn("increment", { type: "int" })
+    id;
+
+    @Column({ name: "content", type: "varchar" })
+    @IsString()
+    @IsUrl()
+    content;
+
+    @CreateDateColumn({ name: "created_at", type: "datetime" })
+    @IsDate()
+    createdAt;
+
+    @UpdateDateColumn({ name: "updated_at", type: "datetime" })
+    @IsDate()
+    updatedAt;
+
+    @DeleteDateColumn({ name: "deleted_at", type: "datetime" })
+    @IsDate()
+    deletedAt;
+
+    @ManyToOne(() => Issue, issue => issue.id)
+    issue;
+    
+    @ManyToOne(() => User, user => user.id)
+    user;
+}
+
+export { Comment };
