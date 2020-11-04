79a792eefea5af4c7273ffacf4a18cfb713ca82d
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-05 01:52:15 +0900

    refactor: [BE] token-generator 환경변수 설정 및 린트 적용
    - 하드 코딩 된 정보를 감추기 위한 환경 변수 설정
    - 린트 적용

--- backend/src/common/lib/token-generator.js
+++ backend/src/common/lib/token-generator.js
@@ -1,11 +1,12 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-const generateJWTToken = (payload, options = {
-    expiresIn: '7d',
-    issuer: 'issueTracker.com',
-    subject: 'userInfo'
-}) => jwt.sign(payload, process.env.JWT_SECRET, options);
+const generateJWTToken = (
+    payload,
+    options = {
+        expiresIn: process.env.JWT_OPTION_TOKEN_EXPIRES_IN,
+        issuer: process.env.JWT_OPTION_TOKEN_ISSUER,
+        subject: process.env.JWT_OPTION_TOKEN_SUBJECT
+    }
+) => jwt.sign(payload, process.env.JWT_SECRET, options);
 
-export {
-    generateJWTToken
-}
+export { generateJWTToken };
