3d1865700e7ae8b2025d46afa8e7d210b63cfed3
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-05 02:09:25 +0900

    refactor: application 환경 설정 작업 추가 및 변경
    - dotenv 환경 변수 참조를 위해 middleware 등록을 별도 함수로 분리
    - authenticator 초기화 작업 추가
    - 쿠키 파싱을 위한 cookieParser 미들웨어 추가
    - session을 사용하기 위한 express-session과 옵션 환경변수화

--- backend/src/application.js
+++ backend/src/application.js
@@ -1,8 +1,11 @@
 import "reflect-metadata";
+import "express-async-errors";
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
-import "express-async-errors";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 import { validateOrReject } from "class-validator";
 import { createConnection } from "typeorm";
 import { errorHandler } from "./common/middleware/error-handler";
@@ -10,24 +13,13 @@ import { router } from "./router";
 import { EnvType } from "./common/env/env-type";
 import { DatabaseEnv } from "./common/env/database-env";
 import { ConnectionOptionGenerator } from "./common/config/database/connection-option-generator";
-import cors from 'cors';
-import { authenticator } from './common/lib';
+import * as authenticator from "./common/lib/authenticator";
 
 export class Application {
     constructor() {
         this.httpServer = express();
-        this.httpServer.use(cors({
-            origin: true,
-            credentials: true
-        }));
-        this.httpServer.use(express.json());
-        this.httpServer.use(express.urlencoded({ extended: false }));
-        this.httpServer.use(router);
-        this.httpServer.use(errorHandler);
-
         this.databaseEnv = null;
         this.connectionOptionGenerator = null;
-        this.authenticator = authenticator;
     }
 
     listen(port) {
@@ -42,7 +34,7 @@ export class Application {
         try {
             await this.initEnvironment();
             await this.initDatabase();
-            this.initAuthenticator();
+            authenticator.initializeAuthenticator(this.httpServer);
         } catch (error) {
             console.error(error);
             process.exit();
@@ -55,20 +47,37 @@ export class Application {
             throw new Error("잘못된 NODE_ENV 입니다. {production, development, local, test} 중 하나를 선택하십시오.");
         }
         dotenv.config({
-            path: path.join(`${process.cwd()}`+`/.env.${process.env.NODE_ENV}`)
+            path: path.join(`${process.cwd()}/.env.${process.env.NODE_ENV}`)
         });
 
         this.databaseEnv = new DatabaseEnv();
         await validateOrReject(this.databaseEnv);
         this.connectionOptionGenerator = new ConnectionOptionGenerator(this.databaseEnv);
+        this.registerMiddleware();
     }
 
     async initDatabase() {
         await createConnection(this.connectionOptionGenerator.generateConnectionOption());
     }
 
-    initAuthenticator(){
-        this.authenticator.setStrategy();
-        this.authenticator.initialize(this.httpServer);
+    registerMiddleware() {
+        this.httpServer.use(
+            cors({
+                origin: true,
+                credentials: true
+            })
+        );
+        this.httpServer.use(cookieParser());
+        this.httpServer.use(express.json());
+        this.httpServer.use(express.urlencoded({ extended: false }));
+        this.httpServer.use(
+            session({
+                secret: process.env.SESSION_SECRET,
+                resave: false,
+                saveUninitialized: true
+            })
+        );
+        this.httpServer.use(router);
+        this.httpServer.use(errorHandler);
     }
 }
