0abe2f6bf2ee2d3032eac8d426d78ba874dd8a1e
jinhyukoo <faill456@naver.com>
2020-11-05 19:38:49 +0900

    feat : [FE] 로그인 라우팅 구현
    - 페이지 별 API 결과를 통한 접근 권한 설정
    - 페이지 별 Context API 설정

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,5 +1,6 @@
 export default {
     API: {
-        POST_LOGIN: process.env.ISSUE_TRACKER_APP_SERVER + process.env.ISSUE_TRACKER_APP_API_POST_LOGIN
+        POST_LOGIN: process.env.ISSUE_TRACKER_APP_SERVER + process.env.ISSUE_TRACKER_APP_API_POST_LOGIN,
+        GET_AUTH: process.env.ISSUE_TRACKER_APP_SERVER + process.env.ISSUE_TRACKER_APP_API_GET_AUTH
     }
 };
