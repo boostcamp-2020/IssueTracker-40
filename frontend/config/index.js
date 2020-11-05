dec8d73145031f20d4984ac2ddd7d042cdb2430c
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-06 00:37:42 +0900

    fix: [FE] 인증된 사용자에 대한 메인 페이지 이동 불가 문제 해결
    - 메인 페이지로 이동하기 위해 API 명세에 따른 URL 경로 수정
    - 쿠키에 대한 요청을 보내기 위해 withCredential 옵션 추가

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,6 +1,6 @@
 export default {
     API: {
-        POST_LOGIN: process.env.ISSUE_TRACKER_APP_SERVER + process.env.ISSUE_TRACKER_APP_API_POST_LOGIN,
+        GET_GITHUB_LOGIN: process.env.ISSUE_TRACKER_APP_SERVER + process.env.ISSUE_TRACKER_APP_API_GET_GITHUB_LOGIN,
         GET_AUTH: process.env.ISSUE_TRACKER_APP_SERVER + process.env.ISSUE_TRACKER_APP_API_GET_AUTH
     }
 };
