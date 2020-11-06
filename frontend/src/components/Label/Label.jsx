e43f704a16316bd074a6c01289378b87425d2ad9
jinhyukoo <faill456@naver.com>
2020-11-06 20:48:23 +0900

    feat: [FE] 배경색에 따른 글자색 변경 기능
    UX 향상을 위해 배경색의 밝기를 측정하여 white와 black 중 적절한 글자색이 설정되게 함

--- frontend/src/components/Label/Label.jsx
+++ frontend/src/components/Label/Label.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
+const defineTextColor = (color) => {
+    const r = parseInt(color.substring(1, 3), 16);
+    const g = parseInt(color.substring(3, 5), 16);
+    const b = parseInt(color.substring(5, 7), 16);
+    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+    if (luminance > 0.5) return "black";
+    return "white";
+};
+
 const StyledLabel = styled.button`
     background-color: ${(props) => props.color};
+    color: ${(props) => defineTextColor(props.color)};
     border-radius: 1rem;
     font-size: 14px;
     font-weight: bold;
