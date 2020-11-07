c3c4db3a1cc37cd04d7b2ba6428cfc4054234ec4
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-08 04:10:57 +0900

    feat: [FE] HeaderContainer 컴포넌트 제작
    - Header의 model 부분을 분리한 Container 컴포넌트 제작
    - header의 렌더링에 필요한 모든 정보를 reducer를 이용해 headerState로
    유지하도록 구현
    - HederContext를 활용해 하위 컴포넌트에서 headerState 접근 및 변경을
    위한 headerState와  이벤트리스너를 전달
    - headerState 변경을 위한 reducer 작성 및 actionType 추가

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,5 @@
 export { default as LoginForm } from "./LoginForm/LoginForm";
 export { default as Button } from "./Button/Button";
+export { default as Header } from "./Header/HeaderContainer/HeaderContainer";
+export { default as UserProfile } from "./UserProfile/UserProfile";
+export { default as Caret } from "./Caret/Caret";
