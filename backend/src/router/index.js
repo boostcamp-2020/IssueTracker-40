017a341889918ec6b8ff6230c4f34a2b4dd0d357
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-08 22:41:57 +0900

    feat: [BE] SignupRouter 추가
    회원가입 요청을 라우팅하기 위한 라우터 추가

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import apiRouter from "./api";
 import oauthRouter from "./oauth";
+import signupRouter from "./signup";
 
 const router = express.Router();
 
 router.use("/api", apiRouter);
 router.use("/oauth", oauthRouter);
+router.use("/signup", signupRouter);
 
 export { router };
