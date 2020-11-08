e0418a4fb4b0ebe95f0e6cfc01b3a0a0b1bfc1da
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-08 22:31:08 +0900

    refactor: [BE] UserService 생성, 조회, 회원가입 함수 추가 및 수정
    - UserService를 통해 새로운 User 객체를 생성할 수 있도록 함수 추가
    	- OAuth를 통해 가입한 회원은 패스워드를 가지지 않으므로 생성
    	함수를 password 인자를 포함한 함수와 포함하지 않은 함수로 분리
    - UserService를 통해 회원가입한 유저인지 검사할 수 있도록 함수 추가
    	- 회원은 이메일, 이름 모두 unique해야 하므로, 두 가지를 모두
    	별도의 함수로 분리
    - 회원가입 함수 추가
    	- 회원가입은 OAuth를 통한 회원가입, 일반 회원가입 모두 절차가
    	다르므로 별도의 회원가입 추가 및 기존 github회원가입 함수 네이밍
    	변경

--- backend/src/service/user-service.js
+++ backend/src/service/user-service.js
@@ -6,12 +6,51 @@ class UserService {
         this.userRepository = getRepository(User);
     }
 
+    createUser({ email, name, password }) {
+        const tempProfileImage = "https://pbs.twimg.com/profile_images/977835673511084032/xXA979th.jpg";
+        const newUser = new User();
+        newUser.email = email;
+        newUser.name = name;
+        newUser.password = password;
+        newUser.profileImage = tempProfileImage;
+
+        return newUser;
+    }
+
+    createUserWithoutPassword({ email, name, profileImage }) {
+        const newUser = new User();
+        newUser.email = email;
+        newUser.name = name;
+        newUser.profileImage = profileImage;
+
+        return newUser;
+    }
+
+    async isUserExistByEmail({ email }) {
+        const user = await this.getUserByEmail(email);
+        return user === undefined;
+    }
+
+    async isUserExistByName({ name }) {
+        const user = await this.getUserByName(name);
+        return user === undefined;
+    }
+
+    async getUserByEmail(useremail) {
+        const user = await this.userRepository.findOne({ email: useremail });
+        return user;
+    }
+
     async getUserByName(username) {
         const user = await this.userRepository.findOne({ name: username });
         return user;
     }
 
-    async signup(profile) {
+    async signup(newUser) {
+        await this.userRepository.save(newUser);
+    }
+
+    async signupWithGitHub(profile) {
         const user = await this.getUserByName(profile.username);
 
         if (!user) {
