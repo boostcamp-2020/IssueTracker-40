74019c72112e5b86873a6433838cb33c42063393
jinhyukoo <faill456@naver.com>
2020-11-08 15:11:04 +0900

    refactor: [FE] return 방식 삼항연산자로 변경
    코드의 가독성을 높이기 위해 early return에서 삼항 연산자 방식으로 변경

--- frontend/src/components/Label/Label.jsx
+++ frontend/src/components/Label/Label.jsx
@@ -6,8 +6,7 @@ const defineTextColor = (color) => {
     const g = parseInt(color.substring(3, 5), 16);
     const b = parseInt(color.substring(5, 7), 16);
     const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
-    if (luminance > 0.5) return "black";
-    return "white";
+    return luminance > 0.5 ? "black" : "white";
 };
 
 const StyledLabel = styled.button`
