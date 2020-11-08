52fb3735912b514f8f5547d16a69cc3eece0e61b
jinhyukoo <faill456@naver.com>
2020-11-09 01:37:27 +0900

    chore: [FE] color에 값 설정
    로딩페이지에 쓰이는 점의 색상 설정

--- frontend/src/common/style/color.js
+++ frontend/src/common/style/color.js
@@ -6,7 +6,8 @@ const color = {
     btn_primary_hover_bg: "#2c974b",
     btn_primary_border: "rgba(27, 31, 35, 0.15)",
     btn_primary_shadow: "rgba(27, 31, 35, 0.04)",
-    btn_primary_text: "#ffffff"
+    btn_primary_text: "#ffffff",
+    loading_dots: "#d8d8d8"
 };
 
 export { color };
