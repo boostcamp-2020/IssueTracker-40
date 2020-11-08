7c9613f10c4175a531215376beeefded3c5f8046
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-09 01:13:50 +0900

    feat: [BE] 패스워드 암호화 모듈 추가
    - 패스워드를 암호화 하기위해 bcrypt를 사용한 모듈 작성
    - 패스워드 암호화, 암호화한 패스워드와 입력받은 패스워드를 비교하는 함수
    작성

---
+++ backend/src/common/lib/crypto.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 10;
+const PIPE = (f, g) => (x, y) => g(y, f(x));
+
+const encrypt = (data) => {
+    const genSalt = (rounds) => bcrypt.genSaltSync(rounds);
+    const hash = (data, salt) => bcrypt.hashSync(data, salt);
+
+    return PIPE(genSalt, hash)(SALT_ROUNDS, data);
+};
+
+const compare = (data, hash) => bcrypt.compareSync(data, hash);
+
+module.exports = {
+    encrypt,
+    compare
+};
