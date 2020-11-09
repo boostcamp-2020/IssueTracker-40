22486e442f468635f11cf0deb5f664824d2c1ba8
jinhyukoo <faill456@naver.com>
2020-11-09 22:37:24 +0900

    chore: [FE] router 경로 재설정
    api를 거치게 재설정

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,13 +2,11 @@ import express from "express";
 import apiRouter from "./api";
 import oauthRouter from "./oauth";
 import signupRouter from "./signup";
-import labelRouter from "./label";
 
 const router = express.Router();
 
 router.use("/api", apiRouter);
 router.use("/oauth", oauthRouter);
 router.use("/signup", signupRouter);
-router.use("/label", labelRouter);
 
 export { router };
