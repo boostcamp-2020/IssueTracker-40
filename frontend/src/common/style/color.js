20b47022139ca6f58e54e36d37f7b0697b0c2928
Jinhyuk Choi <faill456@naver.com>
2020-11-09 10:59:57 +0900

    사이드바 메뉴 컴포넌트 제작 (#63)
    * feat: [FE] SideBarMenu 컴포넌트 UI 제작
    
    * feat: [FE] SideBarMenuHeader에 hover 효과 추가
    
    * chore: [FE] index.js에 SideBarMenu 컴포넌트 경로 추가
    
    * feat: [FE] Header 부분 클릭시 이벤트 발생
    
    props로 함수를 전달해주면 Header 클릭시 특정 이벤트를 발생시킬 수 있게 설정
    
    * chore: [FE] color.js에 색상 추가 및 파일에 적용
    
    * chore: [FE] 웹팩 설정 추가
    
    이미지 파일을 불러오기 위해 file-loader를 웹팩 설정에 추가함
    
    * chore: [FE] 웹팩 설정 변경
    
    - url-loader와 file-loader를 필요한 상황에 따라 사용
    - 10KB 미만일 때는 url-loader, 그 외엔 file-loader 사용
    
    close #14

--- frontend/src/common/style/color.js
+++ frontend/src/common/style/color.js
@@ -7,6 +7,8 @@ const color = {
     btn_primary_border: "rgba(27, 31, 35, 0.15)",
     btn_primary_shadow: "rgba(27, 31, 35, 0.04)",
     btn_primary_text: "#ffffff",
+    sidemenu_hover: "#2e9afe",
+    sidemenu_default: "#6e6e6e",
     header_bg: "#24292e",
     header_title_text: "#ffffff",
     header_dropmenu_bg: "#ffffff",
