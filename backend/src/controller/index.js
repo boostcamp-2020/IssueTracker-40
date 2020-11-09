36d22aed4d93343cef413b21e56e4c480da13a14
jinhyukoo <faill456@naver.com>
2020-11-09 22:00:08 +0900

    feat: [FE] Label 관련 라우터 등록
    현재 생성(post)만 등록된 상태 계속 추가 할 예정

--- backend/src/controller/index.js
+++ backend/src/controller/index.js
@@ -1,4 +1,5 @@
 import * as userController from "./user-controller";
 import * as signupController from "./signup-controller";
+import * as labelController from "./label-controller";
 
-export { userController, signupController };
+export { userController, signupController, labelController };
