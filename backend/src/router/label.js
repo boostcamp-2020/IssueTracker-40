75bf260d928d4a4fdb660ecbdc87bfd9215aab0d
jinhyukoo <faill456@naver.com>
2020-11-10 01:45:18 +0900

    chore: [BE] 라우터에 label 삭제를 위한 경로 추가

--- backend/src/router/label.js
+++ backend/src/router/label.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/", labelController.validateLabelParam, labelController.addLabel);
 router.get("/", labelController.getLabels);
 router.put("/:labelId", labelController.validateLabelParam, labelController.changeLabel);
+router.delete("/:labelId", labelController.removeLabel);
 
 export default router;
