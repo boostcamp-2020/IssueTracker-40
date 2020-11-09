af6c7303565f661022ca93a4cc678675f306d461
jinhyukoo <faill456@naver.com>
2020-11-10 01:26:45 +0900

    chore: [BE] 라우터에 label 수정을 위한 경로 추가

--- backend/src/router/label.js
+++ backend/src/router/label.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/", labelController.validateLabelParam, labelController.addLabel);
 router.get("/", labelController.getLabels);
+router.put("/:labelId", labelController.validateLabelParam, labelController.changeLabel);
 
 export default router;
