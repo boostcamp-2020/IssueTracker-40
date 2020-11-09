697c65b8b7d8450750d65e214a8901e87ececa57
Jinhyuk Choi <faill456@naver.com>
2020-11-09 10:21:10 +0900

    feat: [FE] 라벨 컴포넌트 제작 (#61)
    * feat: [FE] Label Component 제작
    
    - 도호님이 제작해둔 버튼의 스타일을 참고하여 기존 컴포넌트와 비슷한 느낌이 나도록 설계
    - background-color에 따른 글자색 변경 기능 추가 예정
    
    * feat: [FE] 배경색에 따른 글자색 변경 기능
    
    UX 향상을 위해 배경색의 밝기를 측정하여 white와 black 중 적절한 글자색이 설정되게 함
    
    * chore: [FE] Label Component 사용을 위한 index.js 편집
    
    * refactor: [FE] return 방식 삼항연산자로 변경
    
    코드의 가독성을 높이기 위해 early return에서 삼항 연산자 방식으로 변경
    
    close #13

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as LoginForm } from "./LoginForm/LoginForm";
 export { default as Button } from "./Button/Button";
+export { default as Label } from "./Label/Label";
 export { default as Header } from "./Header/HeaderContainer/HeaderContainer";
 export { default as UserProfile } from "./UserProfile/UserProfile";
 export { default as Caret } from "./Caret/Caret";
