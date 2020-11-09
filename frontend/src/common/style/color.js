61adfdf63fef0f5f6ca0082e592a5e6b93266e8d
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-10 02:41:05 +0900

    style: [FE] 색상 추가
    - 컴포넌트가 추가됨에 따른 color 추가

--- frontend/src/common/style/color.js
+++ frontend/src/common/style/color.js
@@ -10,10 +10,12 @@ const color = {
     textarea_bg: "#fafbfc",
     textarea_border: "#e1e4e8",
     textarea_focus_border: "#0366d6",
+    textarea_text: "#586069",
     tab_border: "#e1e4e8",
     tab_bg: "#f6f8fa",
     tab_selected_bg: "#ffffff",
-    tab_container_gb: "#ffffff"
+    tab_container_gb: "#ffffff",
+    border_primary: "#e1e4e8"
 };
 
 export { color };
