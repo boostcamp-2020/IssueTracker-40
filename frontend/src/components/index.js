570c1adb5800008e205960b40b17290b4f1f6bb9
Do Ho Kim <33643752+Do-ho@users.noreply.github.com>
2020-11-10 10:32:55 +0900

    feat: [FE] contentEditor 구현 (#83)
    * feat: [FE] 공통 ContentEditor 구현
    
    - 공통 컴포넌트로 사용하기 위해 구현
    - Textarea를 누를 시 Container의 border를 바꿔주기 위해 useRef를 활용해 구현
    
    * feat: [FE] TabMenu 구현
    
    - 탭에 따른 내용 변경을 위해 useReducer 활용
    
    * chore: [FE] remarkable 설치
    
    - markdown을 표현하기 위한 라이브러리 설치
    
    * fix: [FE] markdown에 영향을 주는 style 수정
    
    - markdown에서 변환되어 나온 태그가 적용되지 않은 문제가 발생하여  h1, h2 등 markdown에 사용되는 태그의 공통으로 적용된 스타일 해제
    - 글자 크기로 인해 줄 간격이 맞지 않은 현상이 발생하여 h2~h6태그 까지 margin-left를 줌
    
    * refactor: [FE] 파일 위치 변경
    
    - 하나의 컴포넌트로 사용하기 위해 파일 위치 변경
    - Tabmenu -> ContentEditor
    - ContentEditor -> Writer
    
    * feat: [FE] Preview 구현
    
    - markdown 형식으로 작성하기 위해 remarkable 사용
    
    * style: [FE] 색상 추가
    
    - 컴포넌트가 추가됨에 따른 color 추가

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as LoginForm } from "./LoginForm/LoginForm";
 export { default as Button } from "./Button/Button";
+export { default as ContentEditor } from "./ContentEditor/ContentEditor";
 export { default as SidebarMenu } from "./SideBarMenu/SideBarMenu";
 export { default as Label } from "./Label/Label";
 export { default as Header } from "./Header/HeaderContainer/HeaderContainer";
