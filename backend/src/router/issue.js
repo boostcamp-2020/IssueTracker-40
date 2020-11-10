51f6fbb14b64e89127df39344fcc33551fa60552
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-11 02:59:36 +0900

    feat: [BE] 댓글 추가/조회 router 구현
    - 사용자의 요청을 받아드리기 위한 router 설정

--- backend/src/router/issue.js
+++ backend/src/router/issue.js
@@ -2,8 +2,8 @@ import express from "express";
 import { RequestType } from "../common/middleware/request-type";
 import { transformer } from "../common/middleware/transformer";
 import { validator } from "../common/middleware/validator";
-import { issueController } from "../controller";
-import { AddIssueRequestBody, UserToIssueRequestParams } from "../dto/issue";
+import { issueController, commentController } from "../controller";
+import { AddIssueRequestBody, UserToIssueRequestParams, CommentRequestParams, AddCommentRequestBody } from "../dto/issue";
 
 const router = express.Router();
 
@@ -21,6 +21,20 @@ router.delete(
     transformer([RequestType.PARAMS], [UserToIssueRequestParams]),
     validator([RequestType.PARAMS]),
     issueController.removeAssignee
+);
+
+router.post(
+    "/:issueId/comment",
+    transformer([RequestType.BODY, RequestType.PARAMS], [AddCommentRequestBody, CommentRequestParams]),
+    validator([RequestType.BODY, RequestType.PARAMS]),
+    commentController.addComment
+);
+
+router.get(
+    "/:issueId/comment",
+    transformer([RequestType.PARAMS], [CommentRequestParams]),
+    validator([RequestType.PARAMS]),
+    commentController.getComments
 );
 
 export default router;
