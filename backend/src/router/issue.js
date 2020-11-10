20c77b3e281c5e6a9d136b7a6e7da3e3aebd1106
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-11 00:42:59 +0900

    feat: [BE] 이슈 담당자 추가/삭제 router 구현
    - 사용자의 요청을 받아드리기 위한 router 설정

--- backend/src/router/issue.js
+++ backend/src/router/issue.js
@@ -2,11 +2,25 @@ import express from "express";
 import { RequestType } from "../common/middleware/request-type";
 import { transformer } from "../common/middleware/transformer";
 import { validator } from "../common/middleware/validator";
-import { AddIssueRequestBody } from "../dto/issue";
 import { issueController } from "../controller";
+import { AddIssueRequestBody, UserToIssueRequestParams } from "../dto/issue";
 
 const router = express.Router();
 
 router.post("/", transformer([RequestType.BODY], [AddIssueRequestBody]), validator([RequestType.BODY]), issueController.addIssue);
+
+router.post(
+    "/:issueId/assignee/:assigneeId",
+    transformer([RequestType.PARAMS], [UserToIssueRequestParams]),
+    validator([RequestType.PARAMS]),
+    issueController.addAssignee
+);
+
+router.delete(
+    "/:issueId/assignee/:assigneeId",
+    transformer([RequestType.PARAMS], [UserToIssueRequestParams]),
+    validator([RequestType.PARAMS]),
+    issueController.removeAssignee
+);
 
 export default router;
