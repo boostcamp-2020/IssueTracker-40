fe8845c9b55394340b3441646ebe6b2d9188b9f9
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-11 00:40:07 +0900

    refactor: [BE] issueController 호출 방식 변경
    - 컨벤션에 따른 controller 호출 로직 변경

--- backend/src/router/issue.js
+++ backend/src/router/issue.js
@@ -2,11 +2,11 @@ import express from "express";
 import { RequestType } from "../common/middleware/request-type";
 import { transformer } from "../common/middleware/transformer";
 import { validator } from "../common/middleware/validator";
-import { addIssue } from "../controller/issue-controller";
 import { AddIssueRequestBody } from "../dto/issue";
+import { issueController } from "../controller";
 
 const router = express.Router();
 
-router.post("/", transformer([RequestType.BODY], [AddIssueRequestBody]), validator([RequestType.BODY]), addIssue);
+router.post("/", transformer([RequestType.BODY], [AddIssueRequestBody]), validator([RequestType.BODY]), issueController.addIssue);
 
 export default router;
