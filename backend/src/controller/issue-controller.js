98f48ad4b1c03bf6b3db8c9d1d1e01ad7f715b2e
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-11 23:03:51 +0900

    feat: [BE] 이슈 마일스톤 생성/삭제 controller 구현
    - issueRouter에 적용하기 위한 controller 구현

--- backend/src/controller/issue-controller.js
+++ backend/src/controller/issue-controller.js
@@ -36,4 +36,26 @@ const removeAssignee = async (req, res, next) => {
     }
 };
 
-export { addIssue, addAssignee, removeAssignee };
+const addMilestone = async (req, res, next) => {
+    const { milestoneId, issueId } = req.params;
+    try {
+        const issueService = IssueService.getInstance();
+        await issueService.addMilestone(milestoneId, issueId);
+        res.status(201).end();
+    } catch (error) {
+        next(error);
+    }
+};
+
+const removeMilestone = async (req, res, next) => {
+    const { milestoneId, issueId } = req.params;
+    try {
+        const issueService = IssueService.getInstance();
+        await issueService.removeMilestone(milestoneId, issueId);
+        res.status(204).end();
+    } catch (error) {
+        next(error);
+    }
+};
+
+export { addIssue, addAssignee, removeAssignee, addMilestone, removeMilestone };
