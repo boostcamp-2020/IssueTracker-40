c56a5823baf09aa7d2459175438a9f2ab96dd8f0
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-12 02:03:15 +0900

    feat: [BE] UserService 테스트 코드 작성
    UserService 코드 품질을 위한 테스트 코드 작성

--- backend/test/service/user-service.test.js
+++ backend/test/service/user-service.test.js
@@ -6,8 +6,40 @@ import { LoginRequestBody } from "../../src/dto/auth";
 import { User } from "../../src/model/user";
 import { BadRequestError } from "../../src/common/error/bad-request-error";
 import { EntityNotFoundError } from "../../src/common/error/entity-not-found-error";
+import { IssueService } from "../../src/service/issue-service";
 
 const mockUser = { email: "test@test.com", name: "테스터", password: "test123!@#", profileImage: "profile image" };
+const mockUsers = [
+    {
+        email: "test1@test.com",
+        name: "테스터1",
+        password: "test123!@#"
+    },
+    {
+        email: "test2@test.com",
+        name: "테스터2",
+        password: "test123!@#"
+    },
+    {
+        email: "test3@test.com",
+        name: "테스터3",
+        password: "test123!@#"
+    },
+    {
+        email: "test4@test.com",
+        name: "테스터4",
+        password: "test123!@#"
+    }
+];
+const mockIssues = [
+    { userId: 1, title: "issue title", content: "issue content" },
+    { userId: 2, title: "issue title2", content: "issue content2" }
+];
+
+const mockIssuesAssignee = [
+    { assigneeId: 3, issueId: 1 },
+    { assigneeId: 4, issueId: 2 }
+];
 
 describe("UserService Test", () => {
     const app = new Application();
@@ -25,8 +57,7 @@ describe("UserService Test", () => {
             await entityManager.query("SAVEPOINT STARTPOINT");
 
             // given
-            const user = entityManager.create(User, mockUser);
-            await userService.signup(user);
+            await userService.signup({ email: mockUser.email, name: mockUser.name, password: mockUser.password });
 
             const loginRequestBody = new LoginRequestBody();
             loginRequestBody.email = mockUser.email;
@@ -36,10 +67,9 @@ describe("UserService Test", () => {
             const authenticatedUser = await userService.authenticate(loginRequestBody);
 
             // then
-            expect(authenticatedUser.id).toBe(user.id);
-            expect(authenticatedUser.name).toBe(user.name);
-            expect(authenticatedUser.password).toBe(user.password);
-            expect(authenticatedUser.profileImage).toBe(user.profileImage);
+            expect(authenticatedUser.id).toBe(1);
+            expect(authenticatedUser.name).toBe(mockUser.name);
+            expect(authenticatedUser.email).toBe(mockUser.email);
 
             await entityManager.query("ROLLBACK TO STARTPOINT");
         });
@@ -94,6 +124,89 @@ describe("UserService Test", () => {
                 // then
                 expect(error instanceof EntityNotFoundError);
             }
+
+            await entityManager.query("ROLLBACK TO STARTPOINT");
+        });
+    });
+
+    test("모든 회원을 조회할 수 있다.", async () => {
+        const userService = UserService.getInstance();
+
+        await TransactionWrapper.transaction(async () => {
+            const entityManager = getEntityManagerOrTransactionManager();
+            await entityManager.query("SAVEPOINT STARTPOINT");
+
+            // given
+            const promises = mockUsers.map(({ email, name, password }) => userService.signup({ email, name, password }));
+            await Promise.all(promises);
+
+            // when
+            const users = await userService.getUsers();
+
+            // then
+            users.sort((user1, user2) => (user1.name > user2.name ? 1 : -1));
+            users.forEach((user, idx) => {
+                expect(user.email).toBe(mockUsers[idx].email);
+                expect(user.name).toBe(mockUsers[idx].name);
+            });
+
+            await entityManager.query("ROLLBACK TO STARTPOINT");
+        });
+    });
+
+    test("Assignee로 배정된 모든 회원을 조회할 수 있다", async () => {
+        const userService = UserService.getInstance();
+        const issueService = IssueService.getInstance();
+
+        await TransactionWrapper.transaction(async () => {
+            const entityManager = getEntityManagerOrTransactionManager();
+            await entityManager.query("SAVEPOINT STARTPOINT");
+
+            // given
+            let promises = mockUsers.map(({ email, name, password }) => userService.signup({ email, name, password }));
+            await Promise.all(promises);
+
+            promises = mockIssues.map(({ userId, title, content }) => issueService.addIssue({ userId, title, content }));
+            await Promise.all(promises);
+
+            promises = mockIssuesAssignee.map(({ assigneeId, issueId }) => issueService.addAssignee(assigneeId, issueId));
+            await Promise.all(promises);
+
+            // when
+            const assignees = await userService.getAssignees();
+
+            // then
+            assignees.forEach((assignee, idx) => {
+                expect(assignee.id).toBe(mockIssuesAssignee[idx].assigneeId);
+                expect(assignee.id).toBe(mockIssuesAssignee[idx].assigneeId);
+            });
+
+            await entityManager.query("ROLLBACK TO STARTPOINT");
+        });
+    });
+
+    test("글을 작성한 모든 회원을 조회할 수 있다", async () => {
+        const userService = UserService.getInstance();
+        const issueService = IssueService.getInstance();
+
+        await TransactionWrapper.transaction(async () => {
+            const entityManager = getEntityManagerOrTransactionManager();
+            await entityManager.query("SAVEPOINT STARTPOINT");
+
+            // given
+            let promises = mockUsers.map(({ email, name, password }) => userService.signup({ email, name, password }));
+            await Promise.all(promises);
+
+            promises = mockIssues.map(({ userId, title, content }) => issueService.addIssue({ userId, title, content }));
+            await Promise.all(promises);
+
+            // when
+            const authors = await userService.getAuthors();
+
+            // then
+            authors.forEach((author, idx) => {
+                expect(author.id).toBe(mockIssues[idx].userId);
+            });
 
             await entityManager.query("ROLLBACK TO STARTPOINT");
         });
