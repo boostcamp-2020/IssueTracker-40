9ff83a8b589740e79e73fdd7bf9bc8b9ae951fcd
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-11 12:52:29 +0900

    test: [BE] issue 댓글 수정/삭제 테스트 코드 추가

--- backend/test/router/comment.test.js
+++ backend/test/router/comment.test.js
@@ -90,4 +90,82 @@ describe("Comment Router Test", () => {
             await entityManager.query("ROLLBACK TO STARTPOINT");
         });
     });
+
+    test("이슈에 댓글 수정 라우팅 테스트", async () => {
+        await TransactionWrapper.transaction(async () => {
+            const entityManager = getEntityManagerOrTransactionManager();
+            await entityManager.query("SAVEPOINT STARTPOINT");
+
+            // given
+            const user = entityManager.create(User, mockUser);
+            await entityManager.save(User, user);
+
+            const token = generateJWTToken({
+                userId: user.id,
+                username: user.name,
+                email: user.email,
+                photos: user.profileImage
+            });
+
+            const issue = entityManager.create(Issue, { ...mockIssue, author: user });
+            await entityManager.save(Issue, issue);
+
+            const commentContent = entityManager.create(CommentContent, mockCommentContent);
+            await entityManager.save(CommentContent, commentContent);
+
+            const comment = entityManager.create(Comment, { user, issue, content: commentContent });
+            await entityManager.save(Comment, comment);
+
+            // when
+            const response = await agent(app.httpServer)
+                .patch(`/api/issue/${issue.id}/comment/${comment.id}`)
+                .set("Cookie", [`token=${token}`])
+                .send({
+                    content: "Changed comment"
+                });
+
+            // then
+            expect(response.status).toEqual(200);
+
+            await entityManager.query("ROLLBACK TO STARTPOINT");
+        });
+    });
+
+    test("이슈에 댓글 삭제 라우팅 테스트", async () => {
+        await TransactionWrapper.transaction(async () => {
+            const entityManager = getEntityManagerOrTransactionManager();
+            await entityManager.query("SAVEPOINT STARTPOINT");
+
+            // given
+            const user = entityManager.create(User, mockUser);
+            await entityManager.save(User, user);
+
+            const token = generateJWTToken({
+                userId: user.id,
+                username: user.name,
+                email: user.email,
+                photos: user.profileImage
+            });
+
+            const issue = entityManager.create(Issue, { ...mockIssue, author: user });
+            await entityManager.save(Issue, issue);
+
+            const commentContent = entityManager.create(CommentContent, mockCommentContent);
+            await entityManager.save(CommentContent, commentContent);
+
+            const comment = entityManager.create(Comment, { user, issue, content: commentContent });
+            await entityManager.save(Comment, comment);
+
+            // when
+            const response = await agent(app.httpServer)
+                .delete(`/api/issue/${issue.id}/comment/${comment.id}`)
+                .set("Cookie", [`token=${token}`])
+                .send();
+
+            // then
+            expect(response.status).toEqual(204);
+
+            await entityManager.query("ROLLBACK TO STARTPOINT");
+        });
+    });
 });
