3b86ef917931312d8c230bd26632580d62a1ab30
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-11 14:06:36 +0900

    feat: [BE] AuthRouter 추가
    인증과 관련된 요청을 라우팅 하기 위한 AuthRouter 추가

---
+++ backend/src/router/auth.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { RequestType } from "../common/middleware/request-type";
+import { transformer } from "../common/middleware/transformer";
+import { validator } from "../common/middleware/validator";
+import { LoginRequestBody } from "../dto/auth";
+import { JwtAuthenticator } from "../common/lib/authenticator";
+import { authController } from "../controller";
+
+const router = express.Router();
+
+router.post("/login", transformer([RequestType.BODY], [LoginRequestBody]), validator([RequestType.BODY]), authController.login);
+
+router.get("/logout", JwtAuthenticator.validateAuthorization, authController.logout);
+
+export default router;
--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import apiRouter from "./api";
 import oauthRouter from "./oauth";
 import signupRouter from "./signup";
+import authRouter from "./auth";
 
 const router = express.Router();
 
 router.use("/api", apiRouter);
 router.use("/oauth", oauthRouter);
+router.use("/auth", authRouter);
 router.use("/signup", signupRouter);
 
 export { router };
