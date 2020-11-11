eba944fef6f6de61ed6592abd69287c2a9e8af59
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-12 02:02:14 +0900

    refactor: [BE] UserController User 타입에 따라 User 정보 조회하도록 수정

--- backend/src/controller/user-controller.js
+++ backend/src/controller/user-controller.js
@@ -1,4 +1,5 @@
 import { UserService } from "../service";
+import { USER_TYPE } from "../common/type";
 
 const sendUserInfo = (req, res, next) => {
     const { user } = req;
@@ -11,10 +12,23 @@ const sendUserInfo = (req, res, next) => {
 
 const sendUsers = async (req, res, next) => {
     try {
+        const { type } = req.query;
         const userService = UserService.getInstance();
-        const users = await userService.getUsers();
 
-        res.status(200).json(users);
+        let users;
+        switch (type) {
+            case USER_TYPE.AUTHUOR:
+                users = await userService.getAuthors();
+                break;
+            case USER_TYPE.ASSIGNEE:
+                users = await userService.getAssignees();
+                break;
+            default:
+                users = await userService.getUsers();
+        }
+
+        const jsonData = users.reduce((acc, cur) => acc.concat({ id: cur.id, name: cur.name }), []);
+        res.status(200).json({ users: jsonData });
     } catch (error) {
         next(error);
     }
