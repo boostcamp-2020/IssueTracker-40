e2036a71aefa94e77e9dc14fd9c9bff097180485
jinhyukoo <faill456@naver.com>
2020-11-11 23:45:16 +0900

    chore: [FE] 경로 설정 추가

---
+++ frontend/src/components/Form/index.js
@@ -0,0 +1,5 @@
+export { default as Container } from "./FormContainer";
+export { default as Input } from "./FormInput";
+export { default as Label } from "./FormLabel";
+export { default as Submit } from "./FormSubmit";
+export { default as WarningMessage } from "./WarningMessage";
