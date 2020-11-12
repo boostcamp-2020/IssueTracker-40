81edca21949dbfb8ff6b6277ec315b831a578f82
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-13 03:05:23 +0900

    feat: [FE] usercontext와 header, 로그인 검사 로직 app으로 이동

--- frontend/src/pages/MilestonePage.jsx
+++ frontend/src/pages/MilestonePage.jsx
@@ -1,25 +1,12 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
-import { usePromise } from "@hook";
-import { UserContext } from "@context";
-import { Header, Main, LabelMilestoneHeader } from "@components";
-import { waitAuthorizationApi } from "@utils";
+import { LabelMilestoneHeader } from "@components";
 
 const MilestonePage = () => {
-    const [loading, resolved, error] = usePromise(waitAuthorizationApi, []);
-
-    if (loading) return <div>로딩중..!</div>;
-    if (error) return <Redirect to="/login" />;
-    if (!resolved) return null;
-
     return (
-        <UserContext.Provider value={resolved.data}>
-            <Header />
-            <Main>
-                <LabelMilestoneHeader value="milestone"/>
-                <p>마일스톤 페이지입니다</p>
-            </Main>
-        </UserContext.Provider>
+        <>
+            <LabelMilestoneHeader value="milestone" />
+            <p>마일스톤 페이지입니다</p>
+        </>
     );
 };
 
--- frontend/src/shared/App.jsx
+++ frontend/src/shared/App.jsx
@@ -1,21 +1,44 @@
 import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { MainPage, LoginPage, IssueDetailPage, LabelPage, MilestonePage, NewIssuePage, NewMilestonePage, SignupPage } from "@pages";
+import { waitAuthorizationApi } from "@utils";
+import { Header, Main } from "@components";
+import { UserContext } from "@context";
+import { usePromise } from "@hook";
+
+const waitAuthorization = () => {
+    if (window.location.pathname !== "/login") {
+        return usePromise(waitAuthorizationApi, []);
+    }
+    return [null, null, null];
+};
 
 const App = () => {
+    const [loading, resolved, error] = waitAuthorization();
+
+    if (window.location.pathname !== "/login") {
+        if (loading) return <div>로딩중..!</div>;
+        if (error) window.location.href = "/login";
+        if (!resolved) return null;
+    }
+
     return (
         <>
-            <Route exact path="/" component={MainPage} />
-            <Switch>
-                <Route path="/login" component={LoginPage} />
-                <Route path="/signup" component={SignupPage} />
-                <Route path="/new" component={NewIssuePage} />
-                <Route path="/issue/:issueId" component={IssueDetailPage} />
-                <Route path="/milestones" component={MilestonePage} />
-                <Route path="/milestones/new" component={NewMilestonePage} />
-                <Route path="/labels" component={LabelPage} />
-                <Redirect from="*" to="/" />
-            </Switch>
+            <UserContext.Provider value={resolved !== null ? resolved.data : {}}>
+                <Route exact path="/login" component={LoginPage} />
+                <Header />
+                <Main>
+                    <Route exact path="/" component={MainPage} />
+                    <Switch>
+                        <Route exact path="/signup" component={SignupPage} />
+                        <Route exact path="/new" component={NewIssuePage} />
+                        <Route exact path="/issue/:issueId" component={IssueDetailPage} />
+                        <Route exact path="/milestones" component={MilestonePage} />
+                        <Route exact path="/milestones/new" component={NewMilestonePage} />
+                        <Route exact path="/labels" component={LabelPage} />
+                    </Switch>
+                </Main>
+            </UserContext.Provider>
         </>
     );
 };
