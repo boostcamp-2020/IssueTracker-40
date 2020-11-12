a9132efdbd6a6b91dbb5facc545a89e4910f074b
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-13 01:30:22 +0900

    refactor: [FE]  withCredentials default로 설정

--- frontend/src/common/utils/api.js
+++ frontend/src/common/utils/api.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 import Config from "@config";
 
+axios.defaults.withCredentials = true;
+
 const postLogin = async (email, password) => {
-    const response = await axios.post(Config.API.POST_LOGIN, { email, password }, { withCredentials: true });
+    const response = await axios.post(Config.API.POST_LOGIN, { email, password });
     return response;
 };
 
 const postSignup = async (email, name, password) => {
-    const response = await axios.post(Config.API.POST_SIGNUP, { email, name, password }, { withCredentials: true });
+    const response = await axios.post(Config.API.POST_SIGNUP, { email, name, password });
+    return response;
+};
     return response;
 };
 
