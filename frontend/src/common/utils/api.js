d6a58d2963a0e0552a00ba7c68cc6250f5b50b18
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-11-13 05:01:17 +0900

    feat: [FE] 이슈조회 API 요청 함수 작성

--- frontend/src/common/utils/api.js
+++ frontend/src/common/utils/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Config from "@config";
+import * as qs from "querystring";
 
 const postLogin = async (email, password) => {
     const response = await axios.post(Config.API.POST_LOGIN, { email, password }, { withCredentials: true });
@@ -11,4 +12,12 @@ const postSignup = async (email, name, password) => {
     return response;
 };
 
-export { postLogin, postSignup };
+const getIssues = async ({ page }) => {
+    const query = qs.stringify({
+        page
+    });
+    const response = await axios.get(`${Config.API.GET_ISSUES}?${query}`, { withCredentials: true });
+    return response;
+};
+
+export { postLogin, postSignup, getIssues };
