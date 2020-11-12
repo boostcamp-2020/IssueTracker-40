ae03d34bf253057f728a96e0727137235f1ed62f
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-13 01:31:04 +0900

    feat: [FE] Label api 요청 함수 작성
    - label 관련 요청을 받기 위해 config에 주소 추가

--- frontend/src/common/utils/api.js
+++ frontend/src/common/utils/api.js
@@ -12,7 +12,25 @@ const postSignup = async (email, name, password) => {
     const response = await axios.post(Config.API.POST_SIGNUP, { email, name, password });
     return response;
 };
+
+const getLabels = async () => {
+    const response = await axios.get(Config.API.LABLE);
     return response;
 };
 
-export { postLogin, postSignup };
+const postLabel = async (data) => {
+    const response = await axios.post(Config.API.LABLE, data);
+    return response;
+};
+
+const patchLabel = async (id) => {
+    const response = await axios.post(`${Config.API.LABLE}/${id}`);
+    return response;
+};
+
+const deleteLabel = async (id) => {
+    const response = await axios.post(`${Config.API.LABLE}/${id}`);
+    return response;
+};
+
+export { postLogin, postSignup, getLabels, postLabel, patchLabel, deleteLabel };
